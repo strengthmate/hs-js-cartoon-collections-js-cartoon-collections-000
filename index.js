058263051e862b6ae4cfad8e15c5b537b0725afc
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-08 22:15:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 function dwarfRollCall(dwarves) {
+  var arr = [];
+  for (let i = 0; i < dwarves.length; i++) {
+    arr.push(`${i+1}. ${dwarves[i]} `);
+  }
+  return arr.join('')
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var arr = [];
+  for (let i = 0; i < planeteerCalls.length; i++) {
+    planeteerCalls[i] = planeteerCalls[i].toUpperCase();
+    arr.push(planeteerCalls[i] + '!');
+  }
+  return arr;
 }
 
 function longPlaneteerCalls(words) {
+  var arr = [];
+  for (let i = 0; i < words.length; i++) {
+  words[i] = words[i].toString();
+    if (words[i].length > 4) {
+      return true;
+    } else {
+      return false;
+      }
+  }
 }
 
 function findTheCheese (foods) {
+  var cheeses = ["cheddar", "gouda", "camembert"];
+  for (let i = 0; i < foods.length; i++) {
+      if (cheeses.includes(foods[i])) {
+      return foods[i];
+      }
+    if (i == foods.length - 1) {
+    return 'no cheese!';
+    }
+  }
 }
